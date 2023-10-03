89e535e7f85f298f94ea1a3a77f0577326a95980
Murad Mammadzade <mammadzade03@gmail.com>
2023-10-03 19:25:09 +0400

    Added seed data which was generated by chatgpt.I have encountered with problem on model builder,therefore i seed data as a .net core 2.1 version.My progress is low,because i am learning new technologies like rabbitmq,mongodb,next etc.

--- src/AuctionService/Data/AuctionDbContext.cs
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -10,4 +10,5 @@ public class AuctionDbContext : DbContext
 
     }
     public DbSet<Auction> Auctions { get; set; }
+    
 }
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -18,5 +18,11 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+try{
+      DbInitializer.InitDb(app);
+}
+catch(Exception e){
+      Console.WriteLine(e);
+}
 
 app.Run();
